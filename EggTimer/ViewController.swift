a43acb331124e0957d31847bce5fb7e866e89faf
Juli Barraza <jubarraza5@gmail.com>
2024-11-29 17:53:08 -0300

    se agrega alarma de sonido y label contador

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -14,10 +15,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var progressBar: UIProgressView!
     
+    @IBOutlet weak var countdownLabel: UILabel!
+    
+    var player = AVAudioPlayer()
+    
     var secondsPassed = 0
     var totalTime = 0
     let eggTimes = ["Soft":300, "Medium":420, "Hard":720]
     var timer = Timer()
+    var secondsRemaining = 60
+
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
@@ -27,6 +34,8 @@ class ViewController: UIViewController {
         progressBar.progress = 0.0
         secondsPassed = 0
         titleLabel.text = hardness
+        secondsRemaining = totalTime
+        countdownLabel.text = String(secondsRemaining)
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector:#selector(updateTimer), userInfo: nil, repeats: true)
         
@@ -36,13 +45,23 @@ class ViewController: UIViewController {
     @objc func updateTimer(){
         if  secondsPassed < totalTime {
             secondsPassed += 1
+            secondsRemaining -= 1
             progressBar.progress = Float(secondsPassed)/Float(totalTime)
+            countdownLabel.text = String(secondsRemaining)
         }
         else{
             timer.invalidate()
             titleLabel.text = "Done!"
+            playSound()
+            countdownLabel.text = "0"
         }
     }
     
+    func playSound() {
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: ".mp3")
+                player = try! AVAudioPlayer(contentsOf: url!)
+                player.play()
+                        
+            }
     
 }
